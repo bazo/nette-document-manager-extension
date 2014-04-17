fe19894b372e3e0e0ef906e74b8a290e84b55df3
Martin Bazik <martin@bazo.sk>
2014-04-17 23:05:32 +0200

    don't rely on constant, hardcoded path to doctrine annotations

--- src/Bazo/MongoDb/DI/DocumentManagerExtension.php
+++ src/Bazo/MongoDb/DI/DocumentManagerExtension.php
@@ -60,7 +60,7 @@ class DocumentManagerExtension extends \Nette\DI\CompilerExtension
 
 
 	/**
-	 * 
+	 *
 	 * @param array $config
 	 * @return \Doctrine\ODM\MongoDB\DocumentManager
 	 */
@@ -86,7 +86,7 @@ class DocumentManagerExtension extends \Nette\DI\CompilerExtension
 
 		$configuration->setMetadataCacheImpl($metadataCache);
 
-		\Doctrine\Common\Annotations\AnnotationRegistry::registerFile(VENDORS_DIR . '/doctrine/mongodb-odm/lib/Doctrine/ODM/MongoDB/Mapping/Annotations/DoctrineAnnotations.php');
+		\Doctrine\Common\Annotations\AnnotationRegistry::registerFile(__DIR__ . '/../../../../../../doctrine/mongodb-odm/lib/Doctrine/ODM/MongoDB/Mapping/Annotations/DoctrineAnnotations.php');
 		if (class_exists('\Gedmo\DoctrineExtensions')) {
 			\Gedmo\DoctrineExtensions::registerAnnotations();
 
