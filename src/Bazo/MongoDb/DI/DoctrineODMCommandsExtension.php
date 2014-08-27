5e05ba23c43f4022286c62abcbcc06fd694a0da0
bazo <martin@bazo.sk>
2014-08-27 14:08:46 +0200

    kdyby/console commands compatibility

--- src/Bazo/MongoDb/DI/DoctrineODMCommandsExtension.php
+++ src/Bazo/MongoDb/DI/DoctrineODMCommandsExtension.php
@@ -26,35 +26,43 @@ class DoctrineODMCommandsExtension extends CompilerExtension
 		// console commands - ODM
 		$container->addDefinition($this->prefix('consoleCommandODMClearMetadataCache'))
 				->setClass('Doctrine\ODM\MongoDB\Tools\Console\Command\ClearCache\MetadataCommand')
-				->addTag('console.command');
+				->addTag('console.command')
+				->addTag('kdyby.console.command');
 
 		$container->addDefinition($this->prefix('consoleCommandODMCreateSchema'))
 				->setClass('Doctrine\ODM\MongoDB\Tools\Console\Command\Schema\CreateCommand')
-				->addTag('console.command');
+				->addTag('console.command')
+				->addTag('kdyby.console.command');
 
 		$container->addDefinition($this->prefix('consoleCommandODMDropSchema'))
 				->setClass('Doctrine\ODM\MongoDB\Tools\Console\Command\Schema\DropCommand')
-				->addTag('console.command');
+				->addTag('console.command')
+				->addTag('kdyby.console.command');
 
 		$container->addDefinition($this->prefix('consoleCommandODMGenerateDocuments'))
 				->setClass('Doctrine\ODM\MongoDB\Tools\Console\Command\GenerateDocumentsCommand')
-				->addTag('console.command');
+				->addTag('console.command')
+				->addTag('kdyby.console.command');
 
 		$container->addDefinition($this->prefix('consoleCommandODMGenerateHydrators'))
 				->setClass('Doctrine\ODM\MongoDB\Tools\Console\Command\GenerateHydratorsCommand')
-				->addTag('console.command');
+				->addTag('console.command')
+				->addTag('kdyby.console.command');
 
 		$container->addDefinition($this->prefix('consoleCommandODMGenerateProxies'))
 				->setClass('Doctrine\ODM\MongoDB\Tools\Console\Command\GenerateProxiesCommand')
-				->addTag('console.command');
+				->addTag('console.command')
+				->addTag('kdyby.console.command');
 
 		$container->addDefinition($this->prefix('consoleCommandODMGenerateRepositories'))
 				->setClass('Doctrine\ODM\MongoDB\Tools\Console\Command\GenerateRepositoriesCommand')
-				->addTag('console.command');
+				->addTag('console.command')
+				->addTag('kdyby.console.command');
 
 		$container->addDefinition($this->prefix('consoleCommandODMQuery'))
 				->setClass('Doctrine\ODM\MongoDB\Tools\Console\Command\QueryCommand')
-				->addTag('console.command');
+				->addTag('console.command')
+				->addTag('kdyby.console.command');
 	}
 
 
